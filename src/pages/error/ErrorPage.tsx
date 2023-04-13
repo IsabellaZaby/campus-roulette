4834ef42ffd9a5e2b612766d255165f19d701b9b
StefanGass <2010859005@fh-burgenland.at>
2023-04-13 10:53:46 +0200

    Added pictures, favicon, error page style

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,8 +1,18 @@
-import './ErrorPage.scss'
+import './ErrorPage.scss';
 
 function ErrorPage() {
-
-    return <div className="error-page-wrapper">Error!</div>
+    return (
+        <div className="error-page-wrapper">
+            <div className="error-container">
+                <h1 className="error-text">Error 404</h1>
+                <img
+                    src="/pictures/sad_unicorn.png"
+                    className="error-img"
+                    alt="Bild eines traurigen Einhorns"
+                />
+            </div>
+        </div>
+    );
 }
 
 export default ErrorPage;
